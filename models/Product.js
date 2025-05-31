71912847f9bf5d506ac8e0db86215f39c92e77de
Rifaatslam <120236681+Rifaatslam@users.noreply.github.com>
2025-05-31 21:44:11 +0600

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,23 @@
-// models/Product.js
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   name: String,
-  price: Number,
   quantity: Number,
+  cost: Number,
   total: Number,
+
+  // Sell related fields
+  sold: {
+    type: Boolean,
+    default: false
+  },
+  sellDate: Date,
+  sellNote: String,
+
+  // Product add করার সময়
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   }
 });
 
